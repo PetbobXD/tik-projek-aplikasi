5f5d06643bd28ad5cd5b48cdc146b2112c4dea20
Peter <peterraphaelunawan22@gmail.com>
2023-02-17 18:46:38 +0700

    update 17 feb

---
+++ index.js
@@ -0,0 +1,156 @@
+var hour = document.getElementById("clock-hour")
+var minutes = document.getElementById("clock-minutes")
+var seconds = document.getElementById("clock-seconds")
+
+var clock = setInterval(
+    function time() {
+        var date_now = new Date();
+        var hr = date_now.getHours();
+        var min = date_now.getMinutes();
+        var sec = date_now.getSeconds();
+
+        if (hr < 10) {
+            hr = "0" + hr;
+        }
+
+        if (min < 10) {
+            min = "0" + min;
+        }
+
+        if (sec < 10) {
+            min = "0" + sec;
+        }
+
+        hour.textContent = hr;
+        minutes.textContent = min;
+        seconds.textContent = sec;
+    }, 1000
+);
+
+function onclickli1() {
+    location.href = "index.html"
+}
+function onclickli2() {
+    location.href = "calender.html"
+}
+function onclickli3() {
+    location.href = "reminder.html"
+}
+function onclickli4() {
+    location.href = "calculator.html"
+}
+function onclickli5() {
+    location.href = "notepad.html"
+}
+function onclickhome() {
+    location.href = "index.html"
+}
+
+let newTask=document.getElementById("newTaskForm")
+let body=document.body
+let addTask=document.getElementById('add-task')
+let Tasks= document.getElementById('Tasks')
+
+
+function onclickadd(){
+    newTask.style.display="block"
+    Tasks.style.display='none'
+}
+
+function formCancel(){
+    newTask.style.display="none"
+    Tasks.style.display='block'
+}
+
+
+//delete button
+var myTasklist = document.getElementsByClassName("li-task");
+var i;
+for (i = 0; i < myTasklist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "delete";
+  span.appendChild(txt);
+  myTasklist[i].appendChild(span);
+}
+
+// Add radio button to li element
+function addRadioButton(li) {
+  var radio = document.createElement("input");
+  radio.type = "radio";
+  radio.name = "taskList";
+  li.prepend(radio);
+  radio.addEventListener("click", function () {
+    li.remove();
+  });
+}
+
+function formSubmit(event){
+    event.preventDefault()
+    newTask.style.display='none'
+    Tasks.style.display='block'
+
+
+// Get the input value and the due date value
+var inputedTasks = document.getElementById("taskInput").value;
+var inputedDueDate = document.getElementById("taskDate").value;
+var inputedDueTime = document.getElementById("taskTime").value;
+
+// Create a new li element
+var li = document.createElement("li");
+li.className='li-task'
+
+// Create a new span element for the to-do text
+var todoSpan = document.createElement("span");
+var todoText = document.createTextNode(inputedTasks);
+todoSpan.appendChild(todoText);
+
+// Create a new span element for the due date
+var dateSpan = document.createElement("span");
+var dateText = document.createTextNode(inputedDueDate);
+dateSpan.appendChild(dateText);
+dateSpan.className='date-span'
+
+
+//create a new span element for the due time
+var timeSpan=document.createElement('span')
+var timeText=document.createTextNode(inputedDueTime)
+timeSpan.appendChild(timeText)
+
+
+// Add the todo and due date spans to the li element
+li.appendChild(todoSpan);
+li.appendChild(dateSpan);
+li.appendChild(timeText);
+
+// If input value is empty, show an alert message, else append the li to the list
+if (inputedTasks === '') {
+    alert("You must write something!");
+} else {
+    document.getElementById("Tasks").appendChild(li);
+}
+
+// Add radio button to li element
+addRadioButton(li);
+
+// Clear the input fields
+document.getElementById("taskInput").value = "";
+document.getElementById("taskDate").value = "";
+document.getElementById("taskTime").value = "";
+
+// Create the close button for the li element
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "delete";
+span.appendChild(txt);
+li.appendChild(span);
+
+// Add the functionality to the close button
+var close = document.getElementsByClassName("delete");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+}
